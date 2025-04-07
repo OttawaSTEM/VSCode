927dce86537e49ecd2163d29aae062afe793bf27
Ottawa STEM Club <49529288+OttawaSTEM@users.noreply.github.com>
2025-04-06 20:14:44 -0400

    Update Microsoft.PowerShell_profile.ps1

--- settings/Windows/Microsoft.PowerShell_profile.ps1
+++ settings/Windows/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,18 @@
 # echo $profile
 # New-Item -Path $profile -Type File -Force
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+$esc = [char]0x1b
+$reset = "$esc[0m"
+$blue = "$esc[94m"
+$cyan = "$esc[96m"
+$green = "$esc[92m"
+$gray = "$esc[90m"
+$magenta = "$esc[95m"
+$red = "$esc[91m"
+$white = "$esc[97m"
+$yellow = "$esc[93m"
+
 function prompt {
   try {
     $GitBranch = git branch --show-current
@@ -8,36 +20,8 @@ function prompt {
   } catch {}
 
   if ($GitBranch) { 
-    $GitBranch = "[`e[33;91m$GitBranch`e[0m]" 
+    $GitBranch = "[$($magenta)$GitBranch$($reset)]"
   }
-  
-  $K8sContext = kubectx -c
-  $K8sNamespace = kubens -c
-  If ($K8sContext -and $K8sNamespace) {
-    $K8S = "[`e[1;33m$K8sContext`:$K8sNamespace`e[0m] `n"
-  }
-  
-  "PS $K8S$PWD$GitBranch> "
-}
 
-Set-Alias k kubectl
-Set-Alias kctx kubectx
-Set-Alias kns kubens
-Set-Alias l dir
-
-# Terraform
-function tfc {
-  terraform console
-}
-function tf {
-  terraform state list
-}
-function tfs {
-  terraform state show
-}
-function tfv {
-  terraform validate
-}
-function tfo {
-  terraform output
+  "PS $PWD $GitBranch>`n"
 }
